d04d167c55d854ead24ecdf7f0b07a82715b6967
Bharath Chandra Thota <bharathchandra.chandra96@gmail.com>
2018-10-26 20:31:40 -0700

    Update Search Inverted Indexes using mongodb

--- Engrapho/views.py
+++ Engrapho/views.py
@@ -1,8 +1,9 @@
-from flask import Flask, render_template, request, redirect, url_for, session
+from flask import Flask, render_template, request, redirect, url_for, session, g
 from werkzeug import secure_filename
 from lxml import etree
 from PyPDF2 import PdfFileReader
-import os, subprocess, sys
+from pymongo import MongoClient
+import os, subprocess, sys, docx
 
 app = Flask(__name__)
 app.secret_key = os.urandom(16)
@@ -10,6 +11,9 @@ UPLOAD_FOLDER ='Files'
 ALLOWED_EXTENSIONS = ['docx', 'pptx', 'mp3', 'pdf', 'epub', 'djvu']
 app.config["UPLOAD_FOLDER"] = UPLOAD_FOLDER
 app.config["XML_FILE"]='project_index.xml'
+g.db = MongoClient('localhost','27017').test_database
+g.collection_main = g.db.test_collection()
+g.collection_inverted = g.db.test_inverted_collection()
 
 @app.route('/',methods=["POST","GET"])
 
@@ -36,46 +40,41 @@ def login():
 # This function searches for the given keyword.
 def search():
     if request.method == 'POST':
-        location=[]
-        search_item = request.form['search'].strip()
+        #The search process is done here.
+        search_items = request.form['search'].strip().split(' ')
+        ids=[]
+        locations=[]
         file_types_selected = request.form.getlist('choices-single-defaul')
-        xml_file = etree.parse('project_index.xml')
-        for fType in file_types_selected:
-            fType=fType.lower()
-            name_as_title = xml_file.xpath("//book[title/text()='"+ search_item +"']/type[@extension='"+ fType +"']/location/text()")
-            name_as_author = xml_file.xpath("//book[author/text()='"+ search_item +"']/type[@extension='"+ fType +"']/location/text()")
-            location.append(name_as_title)
-            location.append(name_as_author)
-            print(location)
-        session['messages']=location
+        for i in search_items:
+            cursor = g.collection_inverted.find({'index':i})
+            for j in cursor:
+                ids = ids+j['ids']
+        ids = list(set(ids))
+        for i in ids:
+            cursor = g.collection_main.find({'_id':i})
+            for j in cursor:
+                locations.append(str(j['location']))
+        session['messages']=locations
     return redirect(url_for('display'))
 
 @app.route('/add',methods=["POST","GET"])
 def add_documents():
 
-    # This function updates xml file.
+    # This function updates the inverted indexes file.
+    def prepocess(meta_data, id):
+        indexes = meta_data['title'].split(' ') + meta_data['author'].split(' ') + meta_data['subtype'].split(' ')
+        print("the indexes are", indexes)
+        for i in indexes:
+            g.collection_inverted.update({'index':i},{'$push':{'ids':{'$each':[id]}}})
+
+
+
+    # This function updates mongodb main file.
     def createOrUpdateBook(meta_data):
-        xml_parsed_file = etree.parse(app.config["XML_FILE"])
-        xml_file = xml_parsed_file.getroot()
-        book_elements = xml_parsed_file.xpath("//book[title/text()='"+ meta_data['bookname'] +"' and author/text()='"+ meta_data['author'] +"']")
-        print(book_elements)
-        if book_elements:
-            root = book_elements[0]
-        else:
-            book = etree.SubElement(xml_file,'book')
-            title = etree.SubElement(book, 'title')
-            title.text = meta_data['bookname']
-            author = etree.SubElement(book, 'author')
-            author.text = meta_data['author']
-            root = book
+        #uploading to the main database.
+        id = g.collection_main.insert(meta_data)
+        prepocess(meta_data, id)
 
-        fileType = etree.SubElement(root,'type')
-        fileType.attrib['extension'] = meta_data['extension']
-        location = etree.SubElement(fileType, 'location')
-        location.text = meta_data['location']
-
-        xmlFile = etree.ElementTree(xml_file)
-        xmlFile.write(app.config["XML_FILE"], pretty_print=True,xml_declaration=True)
 
     # This function extracts meta-data from the file.
     def extractData(extension,location):
@@ -91,6 +90,9 @@ def add_documents():
                 meta_data['author']=file_info['/Author'] if '/Author' in file_info else ''
                 meta_data['bookname']=file_info['/Title'] if '/Title' in file_info else os.path.basename(f.name).split('.')[0]
                 print(meta_data)
+        if extension=='docx' or extension=='docs' or extension=='doc':
+            #extracting data from docx
+
 
         createOrUpdateBook(meta_data)
 
